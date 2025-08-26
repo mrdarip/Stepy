53bed99f170797731df75d167f5828b767533a02
mrdarip <darioelas2005@gmail.com>
2025-08-26 12:55:23 +0200

    fix: add Room annotation to TaskDao functions

--- app/src/main/java/com/mrdarip/stepy/data/local/dao/TaskDao.kt
+++ app/src/main/java/com/mrdarip/stepy/data/local/dao/TaskDao.kt
@@ -3,11 +3,14 @@ package com.mrdarip.stepy.data.local.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.mrdarip.stepy.data.local.entities.TaskEntity
 
 @Dao
 interface TaskDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTask(step: TaskEntity)
+
+    @Query("SELECT * FROM tasks")
     suspend fun getAllTasks(): List<TaskEntity>
 }
