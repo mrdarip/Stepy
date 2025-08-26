64d794071f3687404e8c9a2fa51cff7e56acfbac
mrdarip <darioelas2005@gmail.com>
2025-08-26 22:08:03 +0200

    feat: Integrate Hilt for dependency injection

--- app/src/main/java/com/mrdarip/stepy/MainActivity.kt
+++ app/src/main/java/com/mrdarip/stepy/MainActivity.kt
@@ -13,7 +13,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.mrdarip.stepy.ui.navigation.StepyNavigation
 import com.mrdarip.stepy.ui.theme.StepyTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
