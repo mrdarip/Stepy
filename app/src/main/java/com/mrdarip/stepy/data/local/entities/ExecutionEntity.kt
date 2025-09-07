d277ac334dcd9557cbec4bf58703588e60b63aff
mrdarip <darioelas2005@gmail.com>
2025-09-07 23:23:55 +0200

    fix(typo): rename "entities" table to "executions"

--- app/src/main/java/com/mrdarip/stepy/data/local/entities/ExecutionEntity.kt
+++ app/src/main/java/com/mrdarip/stepy/data/local/entities/ExecutionEntity.kt
@@ -6,7 +6,7 @@ import androidx.room.Index
 import androidx.room.PrimaryKey
 
 @Entity(
-    tableName = "entities",
+    tableName = "executions",
     foreignKeys = [
         ForeignKey(
             entity = ExecutionEntity::class,
