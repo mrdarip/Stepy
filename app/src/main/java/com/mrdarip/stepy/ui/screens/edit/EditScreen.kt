d11146471d4e426381e8bd4d93442e9608ca5ce5
mrdarip <darioelas2005@gmail.com>
2025-09-10 12:30:09 +0200

    feat: Create EditScreen with TextField and save/exit buttons

---
+++ app/src/main/java/com/mrdarip/stepy/ui/screens/edit/EditScreen.kt
@@ -0,0 +1,33 @@
+package com.mrdarip.stepy.ui.screens.edit
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ExecutionScreen(
+) {
+
+    Column {
+        TextField(
+            value = "",
+            onValueChange = { },
+            label = { Text("TaskName") },
+            placeholder = { Text("Name") }
+        )
+
+
+        Row {
+            Button(onClick = { /*TODO*/ }) {
+                Text("Exit")
+            }
+
+            Button(onClick = { /*TODO*/ }) {
+                Text("Save")
+            }
+        }
+    }
+}
