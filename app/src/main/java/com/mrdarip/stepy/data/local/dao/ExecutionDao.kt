fc3182c6bdabf69c77083f61aedcf1cb01c907d9
mrdarip <darioelas2005@gmail.com>
2026-03-21 01:42:51 +0100

    feat: Add method to retrieve executions of specified steps

--- app/src/main/java/com/mrdarip/stepy/data/local/dao/ExecutionDao.kt
+++ app/src/main/java/com/mrdarip/stepy/data/local/dao/ExecutionDao.kt
@@ -3,10 +3,22 @@ package com.mrdarip.stepy.data.local.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.mrdarip.stepy.data.local.entities.ExecutionEntity
 
 @Dao
 interface ExecutionDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertExecution(execution: ExecutionEntity): Long
+
+    @Query(
+        """
+            SELECT * FROM executions
+            WHERE stepId IN (:stepsIds)
+            ORDER BY start DESC -- most recent first
+            LIMIT :count
+        """
+    )
+    fun getExecutionsOfSteps(stepsIds: List<Long>, count: Int): List<ExecutionEntity>
+
 }
